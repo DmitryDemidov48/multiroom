a78747ec64808e42c9f0ccbf38ef62a01647b479
RadKiSSGaS <108529559+DmitryDemidov48@users.noreply.github.com>
2023-09-08 14:32:37 +0300

    first commit

---
+++ js/classes/Sprite.js
@@ -0,0 +1,81 @@
+class Sprite {
+    constructor({
+                    position,
+                    imageSrc,
+                    frameRate = 1,
+                    animations,
+                    frameBuffer = 2,
+                    loop = true,
+                    autoplay = true,
+                    currentAnimation
+                }) {
+        this.position = position
+        this.image = new Image()
+        this.image.onload = () => {
+            this.loaded = true
+            this.width = this.image.width / this.frameRate
+            this.height = this.image.height
+        }
+        this.image.src = imageSrc
+        this.loaded = false
+        this.frameRate = frameRate
+        this.currentFrame = 0
+        this.elapsedFrames = 0
+        this.frameBuffer = frameBuffer
+        this.animations = animations
+        this.loop = loop
+        this.autoplay = autoplay
+        this.currentAnimation = currentAnimation
+
+        if (this.animations) {
+            for (let key in this.animations) {
+                const image = new Image()
+                image.src = this.animations[key].imageSrc
+                this.animations[key].image = image
+            }
+        }
+    }
+    draw () {
+        if (!this.loaded) return
+        const cropBox = {
+            position: {
+                x: this.width * this.currentFrame,
+                y: 0
+            },
+            width: this.width,
+            height: this.height
+        }
+        c.drawImage(
+            this.image,
+            cropBox.position.x,
+            cropBox.position.y,
+            cropBox.width,
+            cropBox.height,
+            this.position.x,
+            this.position.y,
+            this.width,
+            this.height
+        )
+        this.updateFrames()
+    }
+    play() {
+        this.autoplay = true
+    }
+    updateFrames() {
+        if (!this.autoplay) return
+        this.elapsedFrames++
+
+        if (this.elapsedFrames % this.frameBuffer === 0) {
+            if (this.currentFrame < this.frameRate - 1) this.currentFrame++
+            else if (this.loop) this.currentFrame = 0
+        }
+        if (this.currentAnimation?.onComplete) {
+            if (this.currentFrame === this.frameRate - 1 &&
+                !this.currentAnimation.isActive)
+            {
+                this.currentAnimation.onComplete()
+                this.currentAnimation.isActive = true
+            }
+        }
+    }
+}
